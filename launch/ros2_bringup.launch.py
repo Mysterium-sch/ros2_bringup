bac51ce6687268bd28915fc649519d318c01ae2b
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-09 14:00:35 -0400

    add sonar

--- launch/ros2_bringup.launch.py
+++ launch/ros2_bringup.launch.py
@@ -1,7 +1,5 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
@@ -9,46 +7,40 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+    cam_dir = get_package_share_directory('spinnaker_camera_driver')
+    camera_type = LaunchConfiguration('camera_type', default='blackfly_s')
+    serial = LaunchConfiguration('serial', default="'20435009'")
+    sonar = LaunchConfiguration('sonar', default='false')
 
-	cam_dir = get_package_share_directory('spinnaker_camera_driver')
-	camera_type = LaunchConfiguration('camera_type', default='blackfly_s')
-	serial = LaunchConfiguration('serial', default="'20435009'")
-
-
-	included_cam_launch = IncludeLaunchDescription(
-	      PythonLaunchDescriptionSource([os.path.join(
-		 get_package_share_directory('spinnaker_camera_driver'), 'launch'),
-		 '/driver_node.launch.py']),
-	          launch_arguments={'camera_type': camera_type, 'serial': serial, 'computer_brightness': 'true'}.items()
+    included_cam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            cam_dir, 'launch'), '/driver_node.launch.py']),
+        launch_arguments={'camera_type': camera_type, 'serial': serial, 'computer_brightness': computer_brightness}.items()
     )
-		        
-	depth_dir = get_package_share_directory('ms5837_bar_ros')
+        
+    depth_dir = get_package_share_directory('ms5837_bar_ros')
+    included_depth_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            depth_dir, 'launch'), '/bar30.launch.py'])
+    )
+    
+    imu_dir = get_package_share_directory('microstrain_inertial_driver')
+    included_imu_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            imu_dir, 'launch'), '/microstrain_launch.py'])
+    )
+
+    if sonar == 'true':
+        sonar_dir = get_package_share_directory('imagenex831l_ros2')
+        included_sonar_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                sonar_dir, 'launch'), '/sonar.launch.py'])
+        )
+        nodes = [included_cam_launch, included_depth_launch, included_imu_launch, included_sonar_launch]
+    else:
+        nodes = [included_cam_launch, included_depth_launch, included_imu_launch]
+
+    return LaunchDescription(nodes)
 
 
-	included_depth_launch = IncludeLaunchDescription(
-	      PythonLaunchDescriptionSource([os.path.join(
-		 get_package_share_directory('ms5837_bar_ros'), 'launch'),
-		 '/bar30.launch.py'])
-	      )
-	      
-	imu_dir = get_package_share_directory('microstrain_inertial_driver')
-	
-	included_imu_launch = IncludeLaunchDescription(
-	      PythonLaunchDescriptionSource([os.path.join(
-		 get_package_share_directory('microstrain_inertial_driver'), 'launch'),
-		 '/microstrain_launch.py'])
-	      )
-       
-	return LaunchDescription([
-		included_cam_launch,
-		included_depth_launch,
-		included_imu_launch,
-		Node(
-        package='ros2_bringup',
-        namespace='debayer',
-	    executable='debayer.py',
-        name='debayer'
-        ),
-	])
-                
 
