6e704b1282c13234931f986a901a056e12cc4686
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-28 11:31:46 -0400

    fixed image

--- src/debayer.py
+++ src/debayer.py
@@ -14,14 +14,14 @@ class Debayer(Node):
         device = self.get_parameter('device').value
         self.image_pub_grey = self.create_publisher(Image, f'{device}/debayer/image_raw/grey', 10)
         self.image_pub_rgb = self.create_publisher(Image, f'{device}/debayer/image_raw/rgb', 10)
-        self.image_sub = self.create_subscription(Image, f'{device}/flir_camera/image_raw', self.im_callback, 10)
+        self.image_sub = self.create_subscription(Image, f'/flir_camera/image_raw', self.im_callback, 10)
         self.image_sub
 
     def im_callback(self, msg):
         data = msg
         img_in_cv2 = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
         gray = cv.cvtColor(img_in_cv2, cv.COLOR_BayerRG2GRAY)
-        rgb = cv.cvtColor(img_in_cv2, cv.COLOR_BayerRG2RGB)
+        rgb = cv.cvtColor(img_in_cv2, cv.COLOR_BayerRG2BGR)
         gray_image = self.bridge.cv2_to_imgmsg(gray, "mono8")
         rgb_image = self.bridge.cv2_to_imgmsg(rgb, "rgb8")   
         self.image_pub_grey.publish(gray_image)
