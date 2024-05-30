6726f594afb58fe05bfec8ce2b2ed926bdc62bc6
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-30 18:06:41 -0400

    s

--- launch/ros2_bringup_1.launch.py
+++ launch/ros2_bringup_1.launch.py
@@ -3,17 +3,26 @@ import launch
 import ament_index_python
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import LaunchConfigurationEquals, IfCondition
 
 def generate_launch_description():
 
-    _MICROSTRAIN_LAUNCH_FILE = os.path.join(ament_index_python.packages.get_package_share_directory('microstrain_inertial_examples'), 'launch', 'cv7_launch.py')
-    _CV7_PARAMS_FILE = os.path.join(ament_index_python.packages.get_package_share_directory('microstrain_inertial_examples'), 'config', 'cv7', 'cv7.yml')
-    _EMPTY_PARAMS_FILE = os.path.join(get_package_share_directory('microstrain_inertial_driver'),'config','empty.yml')
+    _MICROSTRAIN_LAUNCH_FILE = os.path.join(
+        ament_index_python.packages.get_package_share_directory('microstrain_inertial_examples'),
+        'launch', 'cv7_launch.py'
+    )
+    _CV7_PARAMS_FILE = os.path.join(
+        ament_index_python.packages.get_package_share_directory('microstrain_inertial_examples'),
+        'config', 'cv7', 'cv7.yml'
+    )
+    _EMPTY_PARAMS_FILE = os.path.join(
+        get_package_share_directory('microstrain_inertial_driver'),
+        'config', 'empty.yml'
+    )
 
     camera_type = LaunchConfiguration('camera_type', default='blackfly_s')
     serial = LaunchConfiguration('serial', default="'20435009'")
@@ -22,30 +31,34 @@ def generate_launch_description():
     device = LaunchConfiguration('device', default="")
 
     def get_image():
-        if LaunchConfigurationEquals(device, "jetson_1"):
-            return ['jetson_1/image/compressed']
-        else:
+        if LaunchConfigurationEquals(device, "jetson_2"):
             return ['jetson_2/image/compressed']
+        else:
+            return ['jetson_1/image/compressed']
 
-included_cam_launch = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(os.path.join(cam_dir, 'launch', 'driver_node.launch.py')),
-    launch_arguments={'camera_type': camera_type, 'serial': serial}.items(),
-    remappings=[
-        ('/image_raw', '/jetson_1/image_raw'),
-        ('/camera_info', '/jetson_1/camera_info')
-    ]
-)
+    cam_dir = get_package_share_directory('spinnaker_camera_driver')
+    included_cam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(cam_dir, 'launch', 'driver_node.launch.py')),
+        launch_arguments={'camera_type': camera_type, 'serial': serial}.items()
+    )
+    included_cam_launch_with_namespace = GroupAction(
+        actions=[
+            PushRosNamespace('jetson_1'),
+            included_cam_launch
+        ]
+    )
 
-    #Depth 
+    # Depth 
     ping1d_node = Node(
         package='ms5837_bar_ros',
         executable='bar30_node',
-        output="screen",
-        remappings=[
-        ('/bar30/depth', '/jetson_1/bar30/depth'),
-        ('/bar30/pressure', '/jetson_1/bar30/pressure'),
-        ('/bar30/temperature', '/jetson_1/bar30/temperature')
-    ]
+        output="screen"
+    )
+    ping1d_node_with_namespace = GroupAction(
+        actions=[
+            PushRosNamespace('jetson_1'),
+            ping1d_node
+        ]
     )
 
     base_to_range = Node(
@@ -54,15 +67,23 @@ included_cam_launch = IncludeLaunchDescription(
         output='screen',
         arguments=['0.0', '0.0', '0.0', '0', '0.0', '0.0', 'base_link', 'bar30_link']
     )
+    base_to_range_with_namespace = GroupAction(
+        actions=[
+            PushRosNamespace('jetson_1'),
+            base_to_range
+        ]
+    )
 
     included_imu_launch = IncludeLaunchDescription(
-      PythonLaunchDescriptionSource(_MICROSTRAIN_LAUNCH_FILE),
-         remappings=[
-        ('/imu/data', '/jetson_1/imu/data'),
-        ('/ekf/status', '/jetson_1/ekf/status')
-    ]
+        PythonLaunchDescriptionSource(_MICROSTRAIN_LAUNCH_FILE)
     )
-    
+    included_imu_launch_with_namespace = GroupAction(
+        actions=[
+            PushRosNamespace('jetson_1'),
+            included_imu_launch
+        ]
+    )
+
     sonar_dir = get_package_share_directory('imagenex831l_ros2')
     included_sonar_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(sonar_dir, 'launch', 'sonar.launch.py')),
@@ -82,18 +103,18 @@ included_cam_launch = IncludeLaunchDescription(
         output='screen',
         parameters=[{'cam_topic': cam_topic, 'device': device}]
     )
-   
+
     nodes = [
-        included_cam_launch,
-        ping1d_node,
-        base_to_range,
-        included_imu_launch,
+        included_cam_launch_with_namespace,
+        ping1d_node_with_namespace,
+        base_to_range_with_namespace,
+        included_imu_launch_with_namespace,
         included_sonar_launch,
         included_screen_launch,
         debayer_node
     ]
 
-    topic1 = ['/jetson_2/image/compressed']
+    topic1 = ['/jetson_1/image/compressed']
     topic2 = ['/jetson_1/bar30/depth']
     topic3 = ['/jetson_1/bar30/pressure']
     topic4 = ['/jetson_1/bar30/temperature']
@@ -107,5 +128,7 @@ included_cam_launch = IncludeLaunchDescription(
     return LaunchDescription(
         nodes + [ExecuteProcess(
             cmd = (['ros2', 'bag', 'record'] + all_topics),
-            output = 'screen' )
-    ])
+            output = 'screen'
+        )]
+    )
+
