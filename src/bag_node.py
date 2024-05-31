ea12814ac75d6c807cc37caa9ecc3c2aa12c365f
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-05-31 13:59:29 -0400

    Update bag_node.py

--- src/bag_node.py
+++ src/bag_node.py
@@ -16,7 +16,8 @@ class SimpleBagRecorder(Node):
         self.declare_parameter('depth_topic_d', 'bar30/depth')
         self.declare_parameter('depth_topic_p', 'bar30/pressure')
         self.declare_parameter('depth_topic_t', 'bar30/temperature')
-        self.declare_parameter('sonar_topic', 'imagenex831l/range')
+        self.declare_parameter('sonar_topic_pr', 'imagenex831l/range')
+        self.declare_parameter('sonar_topic_rr', 'imagenex831l/raw_range')
         self.declare_parameter('imu_topic', 'imu/data')
 
         # Get parameter values
@@ -24,7 +25,8 @@ class SimpleBagRecorder(Node):
         depth_topic_d = self.get_parameter('depth_topic_d').get_parameter_value().string_value
         depth_topic_p = self.get_parameter('depth_topic_p').get_parameter_value().string_value
         depth_topic_t = self.get_parameter('depth_topic_t').get_parameter_value().string_value
-        sonar_topic = self.get_parameter('sonar_topic').get_parameter_value().string_value
+        sonar_topic_pr = self.get_parameter('sonar_topic_pr').get_parameter_value().string_value
+        sonar_topic_rr = self.get_parameter('sonar_topic_rr').get_parameter_value().string_value
         imu_topic = self.get_parameter('imu_topic').get_parameter_value().string_value
 
         self.writer = rosbag2_py.SequentialWriter()
@@ -41,7 +43,8 @@ class SimpleBagRecorder(Node):
             (depth_topic_d, 'std_msgs/msg/Float32'),
             (depth_topic_p, 'std_msgs/msg/Float32'),
             (depth_topic_t, 'std_msgs/msg/Float32'),
-            (sonar_topic, 'imagenex831l_ros2/msg/ProcessedRange'),
+            (sonar_topic_pr, 'imagenex831l_ros2/msg/ProcessedRange'),
+            (sonar_topic_rr, 'imagenex831l_ros2/msg/RawRange'),
             (imu_topic, 'sensor_msgs/msg/Imu')
         ]
 
@@ -52,7 +55,7 @@ class SimpleBagRecorder(Node):
                 serialization_format='cdr')
             self.writer.create_topic(topic_info)
 
-        # Create subscriptions
+    #Image
         self.cam_sub_ = self.create_subscription(
             Image,
             cam_topic,
@@ -78,7 +81,12 @@ class SimpleBagRecorder(Node):
     #sonar
         self.sonar_sub_ = self.create_subscription(
             ProcessedRange,
-            sonar_topic,
+            sonar_topic_pr,
+            lambda msg: self.topic_callback(sonar_topic, msg),
+            10)
+        self.sonar_sub_ = self.create_subscription(
+            RawRange,
+            sonar_topic_rr,
             lambda msg: self.topic_callback(sonar_topic, msg),
             10)
     #imu
